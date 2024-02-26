78af9cce97d199e52911b5e591e411360ab047a2
Nick Endle <nick@bigcartel.com>
2024-02-26 11:45:18 -0600

    Add updated cart UX

--- source/theme.js
+++ source/theme.js
@@ -1,6 +1,7 @@
 //= require_directory ./javascripts/vendor
 //= require javascripts/functions
 //= require javascripts/store
+//= require javascripts/cart
 //= require javascripts/product-option-groups
 //= require javascripts/product
 //= require javascripts/product-carousel
