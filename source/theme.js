7fa39fa2785cd086fe1c60f95eab4c67e6fc59a3
Nick Endle <nick@bigcartel.com>
2024-02-26 10:53:19 -0600

    Product grid & PDP updates
    - Add standard product grid settings
    - Add Home settings
    - Add PDP settings

--- source/theme.js
+++ source/theme.js
@@ -1,3 +1,6 @@
 //= require_directory ./javascripts/vendor
+//= require javascripts/functions
 //= require javascripts/store
 //= require javascripts/product-option-groups
+//= require javascripts/product
+//= require javascripts/product-carousel
